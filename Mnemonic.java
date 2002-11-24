ec2ca0fa4131644355cbd55b21403a811cde918d
Philip Roberts <philip.roberts@gmail.com>
2002-11-24 04:35:31 +0000

    Total rewrite - maybe better, maybe not.

--- Mnemonic.java
+++ Mnemonic.java
@@ -2,118 +2,54 @@
 
 /* $Id$ */
 
+import java.util.Arrays;
+
 /**
  * A class to define constant ints to represent mnemonics, and to
  * provide conversion methods between int and String representations.
  */
 public class Mnemonic {
-    
-    public static final int CAL = 0;
-    public static final int INC = 1;
-    public static final int JIF = 2;
-    public static final int JMP = 3;
-    public static final int LCI = 4;
-    public static final int LCR = 5;
-    public static final int LCS = 6;
-    public static final int LDA = 7;
-    public static final int LDI = 8;
-    public static final int LDU = 9;
-    public static final int LDV = 10;
-    public static final int MST = 11;
-    public static final int OPR = 12;
-    public static final int RDI = 13;
-    public static final int RDR = 14;
-    public static final int REH = 15;
-    public static final int SIG = 16;
-    public static final int STI = 17;
-    public static final int STO = 18;
+
+    /**
+     * The heart of the class - an array of mnemonics.
+     * NOTE: must be lexicographically ordered.
+     */
+    private static String[] mnemonicList = {"CAL", "INC", "JIF", "JMP",
+                                            "LCI", "LCR", "LCS", "LDA",
+                                            "LDI", "LDU", "LDV", "MST",
+                                            "OPR", "RDI", "RDR", "REH",
+                                            "SIG", "STI", "STO"};
+
+    public static final int CAL = mnemonicToInt("CAL");
+    public static final int INC = mnemonicToInt("CAL");
+    public static final int JIF = mnemonicToInt("CAL");
+    public static final int JMP = mnemonicToInt("CAL");
+    public static final int LCI = mnemonicToInt("CAL");
+    public static final int LCR = mnemonicToInt("CAL");
+    public static final int LCS = mnemonicToInt("CAL");
+    public static final int LDA = mnemonicToInt("CAL");
+    public static final int LDI = mnemonicToInt("CAL");
+    public static final int LDU = mnemonicToInt("CAL");
+    public static final int LDV = mnemonicToInt("CAL");
+    public static final int MST = mnemonicToInt("CAL");
+    public static final int OPR = mnemonicToInt("CAL");
+    public static final int RDI = mnemonicToInt("CAL");
+    public static final int RDR = mnemonicToInt("CAL");
+    public static final int REH = mnemonicToInt("CAL");
+    public static final int SIG = mnemonicToInt("CAL");
+    public static final int STI = mnemonicToInt("CAL");
+    public static final int STO = mnemonicToInt("CAL");
 
     public static int mnemonicToInt(String m) {
-	if (m.equals("CAL")) {
-	    return CAL;
-	} else if (m.equals("INC")) {
-	    return INC;
-	} else if (m.equals("JIF")) {
-	    return JIF;
-	} else if (m.equals("JMP")) {
-	    return JMP;
-	} else if (m.equals("LCI")) {
-	    return LCI;
-	} else if (m.equals("LCR")) {
-	    return LCR;
-	} else if (m.equals("LCS")) {
-	    return LCS;
-	} else if (m.equals("LDA")) {
-	    return LDA;
-	} else if (m.equals("LDI")) {
-	    return LDI;
-	} else if (m.equals("LDU")) {
-	    return LDU;
-	} else if (m.equals("LDV")) {
-	    return LDV;
-	} else if (m.equals("MST")) {
-	    return MST;
-	} else if (m.equals("OPR")) {
-	    return OPR;
-	} else if (m.equals("RDI")) {
-	    return RDI;
-	} else if (m.equals("RDR")) {
-	    return RDR;
-	} else if (m.equals("REH")) {
-	    return REH;
-	} else if (m.equals("SIG")) {
-	    return SIG;
-	} else if (m.equals("STI")) {
-	    return STI;
-	} else if (m.equals("STO")) {
-	    return STO;
-	} else {
-	    return -1;
-	}
+
+        return Arrays.binarySearch(mnemonicList, m);
     }
 
     public static String intToMnemonic(int i) {
-	switch (i) {
-	case CAL:
-	    return "CAL";
-	case INC:
-	    return "INC";
-	case JIF:
-	    return "JIF";
-	case JMP:
-	    return "JMP";
-	case LCI:
-	    return "LCI";
-	case LCR:
-	    return "LCR";
-	case LCS:
-	    return "LCS";
-	case LDA:
-	    return "LDA";
-	case LDI:
-	    return "LDI";
-	case LDU:
-	    return "LDU";
-	case LDV:
-	    return "LDV";
-	case MST:
-	    return "MST";
-	case OPR:
-	    return "OPR";
-	case RDI:
-	    return "RDI";
-	case RDR:
-	    return "RDR";
-	case REH:
-	    return "REH";
-	case SIG:
-	    return "SIG";
-	case STI:
-	    return "STI";
-	case STO:
-	    return "STO";
-	default:
-	    return "XXX";
-	}
+
+        if(i < mnemonicList.length && i >= 0)
+            return mnemonicList[i];
+
+        return "XXX";
     }
 }
