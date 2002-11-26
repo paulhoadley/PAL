786c45d9b0bd599ef9f568179470983f3aafce19
Paul Hoadley <paulh@logicsquad.net>
2002-11-26 06:43:57 +0000

    Improved Javadoc comments.

--- Code.java
+++ Code.java
@@ -4,9 +4,14 @@
 
 /**
  * A class to represent an instruction and its arguments or operands.
+ *
+ * @version $Revision$
  */
 public class Code {
-    /** The three-letter mnemonic for this instruction. */
+    /** 
+     * The three-letter mnemonic for this instruction.
+     * @see Mnemonic
+     */
     private String mnemonic;
 
     /** The first argument to the instruction. */
@@ -20,6 +25,20 @@ public class Code {
 
     /**
      * Constructor.
+     *
+     * @param mnemonic A <code>String</code> containing the three
+     * letter mnemonic for this <code>Code</code> object.
+     * @param first An <code>int</code> representing the first
+     * argument to the instruction.  If there is no first argument,
+     * this value should be zero.
+     * @param second An <code>Object</code> representing the second
+     * argument to the instruction.  This argument is represented as
+     * an <code>Object</code> because the actual type can be integer,
+     * real or string.  These types are represented by Java
+     * <code>Integer</code>s, <codeFloat</code>s and
+     * <code>String</code>s respectively.
+     * @param lineno the line number in the source file where this
+     * instruction originated.
      */
     public Code(String mnemonic, int first, Object second, int lineno) {
 	this.mnemonic = mnemonic;
@@ -31,6 +50,9 @@ public class Code {
 
     /**
      * Returns the three letter menmonic for this instruction.
+     *
+     * @return A <code>String</code> containing the three letter
+     * mnemonic for this instruction.
      */
     public String getMnemonic() {
 	return mnemonic;
@@ -38,6 +60,9 @@ public class Code {
 
     /**
      * Returns the first argument to this instruction.
+     *
+     * @return An <code>int</code> representing the first argument to
+     * this instruction.
      */
     public int getFirst() {
 	return first;
@@ -45,15 +70,30 @@ public class Code {
 
     /**
      * Returns the second argument to this instruction.
+     *
+     * @return An <code>Object</code> representing the second argument
+     * to this instruction.
+     * @see Code#Code
      */
     public Object getSecond() {
 	return second;
     }
 
+    /**
+     * Returns the line number in the source file where this
+     * instruction originated.
+     *
+     * @return The line number in the source file.
+     */
     public int getLineNo() {
 	return lineno;
     }
 
+    /**
+     * Returns a <code>String</code> representation of this object.
+     *
+     * @return A <code>String</code> representation of this object.
+     */
     public String toString() {
 	return (mnemonic + " " + first + " " + second);
     }
